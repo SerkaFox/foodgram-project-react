e1d401f0adafb139207065f0c397274006fc26a5
serkafox <serkafox@gmail.com>
2023-04-01 18:28:50 +0300

    Флоу1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/приаевт
+# https://docs.djangoproject.com/en/2.2/topics/i18n/приаевтпока
 
 LANGUAGE_CODE = 'en-gb'
 
